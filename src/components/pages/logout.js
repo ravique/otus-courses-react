4042d96772635223a2f51a398d00fc5b6333f675
ravique <andres@space-coding.ru>
2019-10-06 16:21:21 +0300

    Delete session cookie after logout

--- src/components/pages/logout.js
+++ src/components/pages/logout.js
@@ -2,11 +2,13 @@ import React, {Component} from 'react';
 import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {logOut} from "../../actions";
+import API from "../../utils/API";
 
 class DumbLogout extends Component {
 
     componentDidMount() {
-        this.props.dispatch(logOut())
+        this.props.dispatch(logOut());
+        API.post('logout/')
     }
 
     render() {
