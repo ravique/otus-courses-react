2bb08b27190ef6ca8810a5c06fe8a6dc212d5648
ravique <andres@space-coding.ru>
2019-10-18 21:14:02 +0300

    Delete session cookie after logout

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,8 +3,6 @@ import axios from "axios";
 axios.defaults.xsrfCookieName = 'csrftoken';
 axios.defaults.xsrfHeaderName = 'X-CSRFToken';
 
-console.log(process.env.REACT_APP_API_URL);
-
 export default axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
